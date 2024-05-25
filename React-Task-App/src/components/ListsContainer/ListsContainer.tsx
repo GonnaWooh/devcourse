b19d974f66144c115e6d406347197083d95fb07c
GEONWOO <sha_replay@naver.com>
2024-05-25 23:54:03 +0900

    052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525052505250525

---
+++ React-Task-App/src/components/ListsContainer/ListsContainer.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import { IList } from "../../types";
+import List from "../List/List";
+import ActionButton from "../ActionButton/ActionButton";
+import { listsContainer } from "./ListsContainer.css";
+
+type TListsCotainerProps = {
+  boardId: string;
+  lists: IList[];
+};
+
+const ListsContainer: FC<TListsCotainerProps> = ({ lists, boardId }) => {
+  return (
+    <div className={listsContainer}>
+      {lists.map((list) => (
+        <List key={list.listId} list={list} boardId={boardId} />
+      ))}
+      <ActionButton boardId={boardId} listId={""} list />
+    </div>
+  );
+};
+
+export default ListsContainer;
